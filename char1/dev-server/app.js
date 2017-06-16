9c46ac9c02f44ccf4223c660e9fa9143ca23feef
VaJoy Larn <vajoy@qq.com>
2017-06-17 00:54:25 +0800

    new

--- char1/dev-server/app.js
+++ char1/dev-server/app.js
@@ -2,5 +2,7 @@
  * Created by Administrator on 2017/6/13.
  */
 import {init} from './src/js/page/index';
-
+if (module.hot) {
+    module.hot.accept();
+}
 init();
--- char1/dev-server/webpack.config.js
+++ char1/dev-server/webpack.config.js
@@ -2,17 +2,19 @@
  * Created by vajoylarn on 2017/6/13.
  */
 const path = require('path');
-//const webpack = require('webpack');
+const webpack = require('webpack');
 module.exports = {
     entry: './app.js',
     output: {
         publicPath: "/assets/",
-        filename: 'bundle.js',
-        //path: '/'   //只使用 dev-server 可以忽略本属性
+        filename: 'bundle.js'
     },
     devServer: {
         contentBase: path.join(__dirname, "src/html"),
-        compress: true,
-        port: 3333
-    }
+        port: 3333,
+        hot: true
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
 };
